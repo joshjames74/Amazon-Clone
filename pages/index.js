0f32d4d7dca1a65de9a7ad1e4a6c13f0e161a568
Joshua <joshua.tomos.james@gmail.com>
2022-03-16 13:24:15 +0000

    feat: Add Navigation Bar

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import theme from '../themes/default.js';
 import styles from '../styles/Home.module.css'
 import { ChakraProvider } from '@chakra-ui/provider';
 
 import ProductCard from '../components/productCard/productCard.js';
+import NavigationBar from '../components/navigationBar/navigationBar.js';
 import * as product_data from '../samples/productCard.json';
 
 export default function Home() {
 
   return (
-    <ChakraProvider>
+    <ChakraProvider initialColorMode={theme.config.initialColorMode}>
+      <NavigationBar></NavigationBar>
       <ProductCard props={product_data}></ProductCard>
     </ChakraProvider>
   )
