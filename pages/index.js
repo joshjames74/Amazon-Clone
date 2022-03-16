a881a7fe7d1ea51da949008b5034ae12ea7ad463
Joshua <joshua.tomos.james@gmail.com>
2022-03-16 11:59:32 +0000

    Add Chakra-UI

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ChakraProvider } from '@chakra-ui/provider';
+
 import ProductCard from '../components/productCard/productCard.js';
+import * as product_data from '../samples/productCard.json';
 
 export default function Home() {
+
   return (
-    <ProductCard></ProductCard>
+    <ChakraProvider>
+      <ProductCard props={product_data}></ProductCard>
+    </ChakraProvider>
   )
 }
