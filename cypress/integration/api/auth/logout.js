3b4cb8ebcbaf396a6e669eee67697c68927fea22
Joshua James <82473088+joshjames74@users.noreply.github.com>
2022-05-28 20:51:40 +0100

    Delete cypress directory

--- cypress/integration/api/auth/logout.js
+++
@@ -1,8 +0,0 @@
-const _route = 'http://localhost:3000/api/auth/logout';
-
-describe('/api/auth/logout test', () => {
-    it('Visit /api/auth/logout', () => {
-        cy.visit(_route)
-        cy.contains('Oops!, something went wrong')
-    })
-})
--- cypress/integration/api/auth/me.js
+++
@@ -1,8 +0,0 @@
-const _route = 'http://localhost:3000/api/auth/me';
-
-describe('/api/auth/me test', () => {
-    it('Visit /api/auth/me', () => {
-        cy.visit(_route)
-        // Tets
-    })
-})
