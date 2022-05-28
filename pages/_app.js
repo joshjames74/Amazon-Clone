ee9160a9068ffa37f23e4a9dfb929f803f11fe46
Joshua <joshua.tomos.james@gmail.com>
2022-05-28 19:00:05 +0100

    Update API and components

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import '../styles/productCard.css'
+import { React, Component } from 'react';
+import '../styles/globals.css';
+import '../styles/productCard.css';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const { user } = pageProps;
+
+  return (
+    <UserProvider user={user}>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import theme from '../themes/default.js';
-import styles from '../styles/Home.module.css'
 import { ChakraProvider } from '@chakra-ui/provider';
+import { HStack } from '@chakra-ui/react';
 
-import ProductCard from '../components/productCard/productCard.js';
+import ProductCard from '../components/product/productCard.js';
 import NavigationBar from '../components/navigationBar/navigationBar.js';
 import * as product_data from '../samples/productCard.json';
+
+import { getCurrentUserData } from '../utils/user.js';
+import { getCategories } from '../utils/siteData.js';
+import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Home() {
 
   return (
     <ChakraProvider initialColorMode={theme.config.initialColorMode}>
-      <NavigationBar></NavigationBar>
-      <ProductCard props={product_data}></ProductCard>
+      <NavigationBar>
+      </NavigationBar>
+      <HStack>
+        <ProductCard productData={product_data}>
+        </ProductCard>
+      </HStack>
     </ChakraProvider>
   )
 }
